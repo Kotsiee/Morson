1b51f500ba8ee6946c3e47f5a05e86a65849ce85
Kotsiee <66284889+Kotsiee@users.noreply.github.com>
2022-12-12 15:31:00 +0000

    init

---
+++ Model/GetData.php
@@ -0,0 +1,27 @@
+<?php
+
+class GetData
+{
+    protected $data, $filename;
+    public function __construct($f)
+    {
+        $this->filename = $f;
+        $file = fopen($f, "r") or die("could not find file");
+        $this->data = json_decode(fread($file, filesize("Model/index.json")), true);
+    }
+
+    function getData()
+    {
+        return $this->data;
+    }
+
+    function changeData($keys, $curValue, $newValue){
+        foreach ($this->data as $key => $entry) {
+            if ($entry[$keys] == $curValue) {
+                $this->data[$key][$keys] = $newValue;
+            }
+        }
+        $newJsonString = json_encode($this->data, JSON_PRETTY_PRINT);
+        file_put_contents($this->filename, $newJsonString);
+    }
+}
